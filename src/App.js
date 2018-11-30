24a42ed2927cc3e537b791a9d6814fb37c932915
Cristina Tarantino <cristina@tarantino.io>
2018-11-30 19:08:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person_list: [
+        { name: 'Mark', age: 46 },
+        { name: 'Cristina', age: 32 },
+        { name: 'Francesco', age: 1.5 }
+      ]
+    };
+  }
+  
+  // https://medium.freecodecamp.org/the-best-way-to-bind-event-handlers-in-react-282db2cf1530
+  // you could do switchNameHandler() {...} but the you would need to
+  // write in the constructor this.switchNameHandler = this.switchNameHandler.bind(this);
+  switchNameHandler = () => {
+    this.setState({
+      person_list: [
+        { name: 'Mark Richard', age: 46 },
+        { name: 'Maria Cristina', age: 32 },
+        { name: 'Francesco Shaun', age: 1.5 }
+      ]
+    });
+  };
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I am a Ferguson React App </h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.person_list[0].name} age={this.state.person_list[0].age}/>
+        <Person name={this.state.person_list[1].name} age={this.state.person_list[1].age}>My Hobbies: Watching sport</Person>
+        <Person name={this.state.person_list[2].name} age={this.state.person_list[2].age}>My Hobbies: Drawing</Person>
       </div>
     );
   }
